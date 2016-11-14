18ded8fcf3d43fa73e66047e31ac526ea566da78
joshfullmer <joshfullmer@me.com>
2016-11-14 08:23:43 -0700

    Remove old attempts

--- rcsvconverter.R
+++ rcsvconverter.R
@@ -8,8 +8,3 @@ xmllist <- xmlToList(xml)
 df <- ldply(xmllist,data.frame)
 
 write.csv(df,"./data/testxml.csv",row.names = F,na="")
-
-#xmllist <- t(xmllist)
-#xmldf <- as.data.frame(xmllist,row.names = T)
-
-#test <- cbind(xmldf,t(as.data.frame(lapply(xmldf$batters,unlist))))
